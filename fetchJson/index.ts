d7413923c100568db8c5bc68c667a1dafefd0126
hyudundee <hyudundee@gmail.com>
2021-03-18 22:08:46 -0700

    two kind of type errors

--- fetchJson/index.ts
+++ fetchJson/index.ts
@@ -12,6 +12,17 @@ axios.get(url).then(response => {
   const todo = response.data as Todo
 
   const id = todo.id
+  // const id = todo.ID
   const title = todo.title
   const completed = todo.completed
+  // logTodo(completed, id, title)
+  logTodo(id, title, completed)
 })
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`
+    The Todo with ID: ${id}
+    Has a title of: ${title}
+    Is it finished? ${completed}
+  `)
+}
